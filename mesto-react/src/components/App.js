585ac0ee7aedabd50f0d4d061d6d66a13ca78b6a
Kustik88 <Bobrenko88@yandex.ru>
2023-04-01 14:45:45 +0700

    feat: добавить разметку попапов в App

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -6,6 +6,70 @@ import Footer from './Footer'
 function App() {
   return (
     <div className="page">
+      <section class="popup popup_type_profile-edit">
+        <div class="popup__container">
+          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form class="popup__form" name="popup-form" novalidate>
+            <h2 class="popup__heading">Редактировать профиль</h2>
+            <div class="popup__input-container">
+              <input type="text" class="popup__input" name="owner" id="owner-input" placeholder="Имя" minlength="2"
+                maxlength="40" required />
+              <span class="popup__input-error" id="owner-input-error"></span>
+              <input type="text" class="popup__input" name="job" id="job-input" placeholder="О себе" minlength="2"
+                maxlength="200" required />
+              <span class="popup__input-error" id="job-input-error"></span>
+            </div>
+            <button type="submit" class="popup__submit-btn"></button>
+          </form>
+        </div>
+      </section>
+      <section class="popup popup_type_avatar-edit">
+        <div class="popup__container popup__container_size_average">
+          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form class="popup__form" name="popup-form" novalidate>
+            <h2 class="popup__heading">Обновить аватар</h2>
+            <div class="popup__input-container">
+              <input type="url" class="popup__input" name="avatar" id="avatar-input" placeholder="Ссылка на картинку"
+                required />
+              <span class="popup__input-error" id="avatar-input-error"></span>
+            </div>
+            <button type="submit" class="popup__submit-btn"></button>
+          </form>
+        </div>
+      </section>
+      <section class="popup popup_type_add-card">
+        <div class="popup__container">
+          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form class="popup__form" name="popup-form" novalidate>
+            <h2 class="popup__heading">Новое место</h2>
+            <div class="popup__input-container">
+              <input type="text" class="popup__input" name="name" id="title-input" placeholder="Название" minlength="2"
+                maxlength="30" required />
+              <span class="popup__input-error" id="title-input-error"></span>
+              <input type="url" class="popup__input" name="link" id="url-input" placeholder="Ссылка на картинку" required />
+              <span class="popup__input-error" id="url-input-error"></span>
+            </div>
+            <button type="submit" class="popup__submit-btn"></button>
+          </form>
+        </div>
+      </section>
+      <section class="popup popup_type_delete-card">
+        <div class="popup__container popup__container_size_small">
+          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form class="popup__form" name="popup-form">
+            <h2 class="popup__heading">Вы уверены?</h2>
+            <button type="submit" class="popup__submit-btn">Да</button>
+          </form>
+        </div>
+      </section>
+      <section class="popup popup_type_image popup_background-blackout_hard">
+        <figure class="popup__figure">
+          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <img class="popup__image" src="#" alt="картинка" />
+          <figcaption class="popup__caption"></figcaption>
+        </figure>
+      </section>
+
       <Header />
       <Main />
       <Footer />
--- mesto-react/src/components/Main.js
+++ mesto-react/src/components/Main.js
@@ -4,13 +4,16 @@ function Main() {
     return (
         <main className="content">
             <section className="profile">
-                <img className="profile__avatar" src="./images/avatar-kusto.jpg" alt="Жак-Ив Кусто" />
+                <button className="profile__edit-avatar-btn" aria-label="Изменить аватар" type="button">
+                    <img className="profile__avatar" src="#" alt="#" />
+                    <div className="profile__overlay-avatar-btn"></div>
+                </button>
                 <div className="profile__info">
                     <div className="profile__owner-container">
-                        <h1 className="profile__owner">Жак-Ив Кусто</h1>
+                        <h1 className="profile__owner"></h1>
                         <button className="profile__edit-btn" aria-label="Изменить профиль" type="button"></button>
                     </div>
-                    <p className="profile__job">Исследователь океана</p>
+                    <p className="profile__job"></p>
                 </div>
                 <button className="profile__add-btn" aria-label="Добавить место" type="button">
                 </button>
