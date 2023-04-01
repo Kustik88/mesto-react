b18b19004bb709f4a4649258c3c229f562c17fed
Kustik88 <Bobrenko88@yandex.ru>
2023-04-01 16:12:34 +0700

    feat: добавить функции-обработчики кнопок

--- src/components/App.js
+++ src/components/App.js
@@ -6,67 +6,67 @@ import Footer from './Footer'
 function App() {
   return (
     <div className="page">
-      <section class="popup popup_type_profile-edit">
-        <div class="popup__container">
-          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
-          <form class="popup__form" name="popup-form" novalidate>
-            <h2 class="popup__heading">Редактировать профиль</h2>
-            <div class="popup__input-container">
-              <input type="text" class="popup__input" name="owner" id="owner-input" placeholder="Имя" minlength="2"
-                maxlength="40" required />
-              <span class="popup__input-error" id="owner-input-error"></span>
-              <input type="text" class="popup__input" name="job" id="job-input" placeholder="О себе" minlength="2"
-                maxlength="200" required />
-              <span class="popup__input-error" id="job-input-error"></span>
+      <section className="popup popup_type_profile-edit">
+        <div className="popup__container">
+          <button className="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form className="popup__form" name="popup-form" noValidate>
+            <h2 className="popup__heading">Редактировать профиль</h2>
+            <div className="popup__input-container">
+              <input type="text" className="popup__input" name="owner" id="owner-input" placeholder="Имя" minLength="2"
+                maxLength="40" required />
+              <span className="popup__input-error" id="owner-input-error"></span>
+              <input type="text" className="popup__input" name="job" id="job-input" placeholder="О себе" minLength="2"
+                maxLength="200" required />
+              <span className="popup__input-error" id="job-input-error"></span>
             </div>
-            <button type="submit" class="popup__submit-btn"></button>
+            <button type="submit" className="popup__submit-btn">Сохранить</button>
           </form>
         </div>
       </section>
-      <section class="popup popup_type_avatar-edit">
-        <div class="popup__container popup__container_size_average">
-          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
-          <form class="popup__form" name="popup-form" novalidate>
-            <h2 class="popup__heading">Обновить аватар</h2>
-            <div class="popup__input-container">
-              <input type="url" class="popup__input" name="avatar" id="avatar-input" placeholder="Ссылка на картинку"
+      <section className="popup popup_type_avatar-edit">
+        <div className="popup__container popup__container_size_average">
+          <button className="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form className="popup__form" name="popup-form" noValidate>
+            <h2 className="popup__heading">Обновить аватар</h2>
+            <div className="popup__input-container">
+              <input type="url" className="popup__input" name="avatar" id="avatar-input" placeholder="Ссылка на картинку"
                 required />
-              <span class="popup__input-error" id="avatar-input-error"></span>
+              <span className="popup__input-error" id="avatar-input-error"></span>
             </div>
-            <button type="submit" class="popup__submit-btn"></button>
+            <button type="submit" className="popup__submit-btn">Сохранить</button>
           </form>
         </div>
       </section>
-      <section class="popup popup_type_add-card">
-        <div class="popup__container">
-          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
-          <form class="popup__form" name="popup-form" novalidate>
-            <h2 class="popup__heading">Новое место</h2>
-            <div class="popup__input-container">
-              <input type="text" class="popup__input" name="name" id="title-input" placeholder="Название" minlength="2"
-                maxlength="30" required />
-              <span class="popup__input-error" id="title-input-error"></span>
-              <input type="url" class="popup__input" name="link" id="url-input" placeholder="Ссылка на картинку" required />
-              <span class="popup__input-error" id="url-input-error"></span>
+      <section className="popup popup_type_add-card">
+        <div className="popup__container">
+          <button className="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form className="popup__form" name="popup-form" noValidate>
+            <h2 className="popup__heading">Новое место</h2>
+            <div className="popup__input-container">
+              <input type="text" className="popup__input" name="name" id="title-input" placeholder="Название" minLength="2"
+                maxLength="30" required />
+              <span className="popup__input-error" id="title-input-error"></span>
+              <input type="url" className="popup__input" name="link" id="url-input" placeholder="Ссылка на картинку" required />
+              <span className="popup__input-error" id="url-input-error"></span>
             </div>
-            <button type="submit" class="popup__submit-btn"></button>
+            <button type="submit" className="popup__submit-btn">Сохранить</button>
           </form>
         </div>
       </section>
-      <section class="popup popup_type_delete-card">
-        <div class="popup__container popup__container_size_small">
-          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
-          <form class="popup__form" name="popup-form">
-            <h2 class="popup__heading">Вы уверены?</h2>
-            <button type="submit" class="popup__submit-btn">Да</button>
+      <section className="popup popup_type_delete-card">
+        <div className="popup__container popup__container_size_small">
+          <button className="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <form className="popup__form" name="popup-form">
+            <h2 className="popup__heading">Вы уверены?</h2>
+            <button type="submit" className="popup__submit-btn">Да</button>
           </form>
         </div>
       </section>
-      <section class="popup popup_type_image popup_background-blackout_hard">
-        <figure class="popup__figure">
-          <button class="popup__close-btn" aria-label="Закрыть" type="button"></button>
-          <img class="popup__image" src="#" alt="картинка" />
-          <figcaption class="popup__caption"></figcaption>
+      <section className="popup popup_type_image popup_background-blackout_hard">
+        <figure className="popup__figure">
+          <button className="popup__close-btn" aria-label="Закрыть" type="button"></button>
+          <img className="popup__image" src="#" alt="картинка" />
+          <figcaption className="popup__caption"></figcaption>
         </figure>
       </section>
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,54 @@
 import React from 'react';
 
+function openPopup(selectorPopup) {
+    document.querySelector(selectorPopup).classList.add('popup_opened')
+}
+
+function handleEditAvatarClick() {
+    openPopup('.popup_type_avatar-edit')
+}
+
+function handleEditProfileClick() {
+    openPopup('.popup_type_profile-edit')
+}
+
+function handleAddPlaceClick() {
+    openPopup('.popup_type_add-card')
+}
+
+
 function Main() {
     return (
         <main className="content">
             <section className="profile">
-                <button className="profile__edit-avatar-btn" aria-label="Изменить аватар" type="button">
+                <button
+                    className="profile__edit-avatar-btn"
+                    aria-label="Изменить аватар"
+                    type="button"
+                    onClick={handleEditAvatarClick}
+                >
                     <img className="profile__avatar" src="#" alt="#" />
                     <div className="profile__overlay-avatar-btn"></div>
                 </button>
                 <div className="profile__info">
                     <div className="profile__owner-container">
-                        <h1 className="profile__owner"></h1>
-                        <button className="profile__edit-btn" aria-label="Изменить профиль" type="button"></button>
+                        <h1 className="profile__owner">Жак</h1>
+                        <button
+                            className="profile__edit-btn"
+                            aria-label="Изменить профиль"
+                            type="button"
+                            onClick={handleEditProfileClick}
+                            >
+                        </button>
                     </div>
                     <p className="profile__job"></p>
                 </div>
-                <button className="profile__add-btn" aria-label="Добавить место" type="button">
+                <button
+                    className="profile__add-btn"
+                    aria-label="Добавить место"
+                    type="button"
+                    onClick={handleAddPlaceClick}
+                >
                 </button>
             </section>
             <ul className="cards">
